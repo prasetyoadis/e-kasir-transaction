405072a4519b3a8e57d12cad840690cff08e0b1c
unknown <erlangganiko42@gmail.com>
2026-02-04 18:52:20 +0700

    update web kasir

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+Route::get('/dashboard/kasir', function () {
+    return view('dashboard.cashier-pos.index');
+});
+Route::get('/dashboard/kasir-cafe', function () {
+    return view('dashboard.cashier-cafe.index');
+});
+
